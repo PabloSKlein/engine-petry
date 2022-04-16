555b24933430e784f71eb7270db21ed87eac41d0
pablo_kleim <pablo.190777@gmail.com>
2022-04-16 13:29:52 -0300

    commit inicial

---
+++ src/main/java/com/unisinos/enginepetry/EnginePetryApplication.java
@@ -0,0 +1,13 @@
+package com.unisinos.enginepetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnginePetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnginePetryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unisinos/enginepetry/EnginePetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisinos.enginepetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnginePetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
