735b80f5449d2231606d75c34a8024e088d07144
pablo_kleim <pablo.190777@gmail.com>
2022-04-16 15:55:54 -0300

    impl main teste

--- src/main/java/com/unisinos/enginepetry/EnginePetryApplication.java
+++ src/main/java/com/unisinos/enginepetry/EnginePetryApplication.java
@@ -2,7 +2,11 @@ package com.unisinos.enginepetry;
 
 import com.unisinos.enginepetry.model.Lugar;
 import com.unisinos.enginepetry.model.RedePetry;
+import com.unisinos.enginepetry.model.Transicao;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static com.unisinos.enginepetry.model.TipoConexaoEnum.CONSUMO;
+import static com.unisinos.enginepetry.model.TipoConexaoEnum.GERACAO;
 
 @SpringBootApplication
 public class EnginePetryApplication {
@@ -11,6 +15,14 @@ public class EnginePetryApplication {
         var redePetry = new RedePetry();
         //L 1 0
         redePetry.adicionarLugar(new Lugar("1", 0));
+        //T 1
+        redePetry.adicionarTransicao(new Transicao("1"));
+        //L 2 0
+        redePetry.adicionarLugar(new Lugar("2", 0));
+        //C C L1 T1
+        redePetry.adicionarConexao(1, CONSUMO, "L1", "T1");
+        //C G T1 L2
+        redePetry.adicionarConexao(1, GERACAO, "L2", "T1");
     }
 
 }
